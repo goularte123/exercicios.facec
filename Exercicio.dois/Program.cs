e149215557ce780dc2d44bf0797b61c47a0c0e9e
goularte123 <marcelogabrielsantanna@gmail.com>
2022-03-02 21:23:41 -0300

    exer

---
+++ Exercicio.dois/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicio.dois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ exercecio.um/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercecio.um
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("informe um valor em R$: ");
+            string valorDigitado = Console.ReadLine();
+            Decimal valorConvertido = 0.0m;
+            decimal valorCambio = 0.1932m;  
+
+            decimal.TryParse(valorDigitado, out valorConvertido);
+            decimal valorCabioConvertido = valorConvertido / valorCambio;
+
+            Console.WriteLine($"valor em $:{valorCabioConvertido }");
+            Console.ReadLine();
+        }
+    }
+}
